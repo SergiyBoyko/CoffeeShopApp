9cdce3884a0915619a8a66ab157d2d1ff01c407f
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-12-06 14:21:19 +0200

    Added BaseView class

---
+++ app/src/main/java/com/example/android/coffeeshopapp/views/BaseView.java
@@ -0,0 +1,13 @@
+package com.example.android.coffeeshopapp.views;
+
+import android.content.Context;
+
+/**
+ * Created by dev_serhii on 06.12.2017.
+ */
+
+public interface BaseView {
+
+    Context getContext();
+
+}

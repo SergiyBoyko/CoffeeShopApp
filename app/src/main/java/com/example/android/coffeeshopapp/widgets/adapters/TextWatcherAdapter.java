c25460f86563e2668abcc233371ad65c696b718b
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-12-11 19:45:26 +0200

    Added TextWatcherAdapter for EditText view

---
+++ app/src/main/java/com/example/android/coffeeshopapp/widgets/adapters/TextWatcherAdapter.java
@@ -0,0 +1,42 @@
+package com.example.android.coffeeshopapp.widgets.adapters;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+/**
+ * Created by dev_serhii on 07.12.2017.
+ */
+
+public class TextWatcherAdapter implements TextWatcher {
+
+    public interface TextWatcherListener {
+
+        void onTextChanged(EditText view, String text);
+
+    }
+
+    private final EditText view;
+    private final TextWatcherListener listener;
+
+    public TextWatcherAdapter(EditText editText, TextWatcherListener listener) {
+        this.view = editText;
+        this.listener = listener;
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        listener.onTextChanged(view, s.toString());
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        // pass
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        // pass
+    }
+
+}

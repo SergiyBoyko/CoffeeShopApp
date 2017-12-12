7e1e70e271f7ce96acfd160cbb0ccda1889d273d
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-12-12 18:43:50 +0200

    Added model class

---
+++ app/src/main/java/com/example/android/coffeeshopapp/model/entities/ResponseEntity.java
@@ -0,0 +1,68 @@
+package com.example.android.coffeeshopapp.model.entities;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by dev_serhii on 12.12.2017.
+ */
+
+public class ResponseEntity {
+
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("firstName")
+    @Expose
+    private String firstName;
+    @SerializedName("secondName")
+    @Expose
+    private String secondName;
+    @SerializedName("email")
+    @Expose
+    private String email;
+    @SerializedName("balance")
+    @Expose
+    private Double balance;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+}

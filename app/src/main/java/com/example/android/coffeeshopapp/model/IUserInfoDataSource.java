c2c0a845872164b6a1c8b92ca0cea9056f0de9c9
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-12-12 18:35:35 +0200

    Added UserInfo data source

---
+++ app/src/main/java/com/example/android/coffeeshopapp/model/IUserInfoDataSource.java
@@ -0,0 +1,15 @@
+package com.example.android.coffeeshopapp.model;
+
+import com.example.android.coffeeshopapp.model.entities.ResponseEntity;
+
+import rx.Observable;
+
+/**
+ * Created by dev_serhii on 12.12.2017.
+ */
+
+public interface IUserInfoDataSource {
+
+    Observable<ResponseEntity> getUserData(int id);
+
+}
---
+++ app/src/main/java/com/example/android/coffeeshopapp/model/remote/UserInfoRemoteDataSource.java
@@ -0,0 +1,25 @@
+package com.example.android.coffeeshopapp.model.remote;
+
+import com.example.android.coffeeshopapp.api.CoffeeShopApi;
+import com.example.android.coffeeshopapp.model.IUserInfoDataSource;
+import com.example.android.coffeeshopapp.model.entities.ResponseEntity;
+
+import rx.Observable;
+
+/**
+ * Created by dev_serhii on 12.12.2017.
+ */
+
+public class UserInfoRemoteDataSource implements IUserInfoDataSource {
+
+    CoffeeShopApi coffeeShopApi;
+
+    public UserInfoRemoteDataSource(CoffeeShopApi coffeeShopApi) {
+        this.coffeeShopApi = coffeeShopApi;
+    }
+
+    @Override
+    public Observable<ResponseEntity> getUserData(int id) {
+        return coffeeShopApi.getUserData(id);
+    }
+}

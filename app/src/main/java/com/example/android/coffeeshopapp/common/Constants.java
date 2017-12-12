5d907514570e60aad8a307c5e1329ee49c407f9e
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-12-12 21:56:58 +0200

    Added constants

--- app/src/main/java/com/example/android/coffeeshopapp/common/Constants.java
+++ app/src/main/java/com/example/android/coffeeshopapp/common/Constants.java
@@ -8,4 +8,8 @@ public class Constants {
 
     public static final String COFFEE_SHOP_TEST_API = "https://coffee-shop-test.herokuapp.com";
 
+    public static final String CARD_ID = "card_id";
+
+    public static final String BALANCE = "balance";
+
 }

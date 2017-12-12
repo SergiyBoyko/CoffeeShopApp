5f2cf7f34f65b6fb41cfec3f59875a3b7d703c2b
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-12-12 22:03:28 +0200

    Refactored model fields type

--- app/src/main/java/com/example/android/coffeeshopapp/model/entities/ResponseEntity.java
+++ app/src/main/java/com/example/android/coffeeshopapp/model/entities/ResponseEntity.java
@@ -11,7 +11,7 @@ public class ResponseEntity {
 
     @SerializedName("id")
     @Expose
-    private Integer id;
+    private Long id;
     @SerializedName("firstName")
     @Expose
     private String firstName;
@@ -25,11 +25,11 @@ public class ResponseEntity {
     @Expose
     private Double balance;
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 

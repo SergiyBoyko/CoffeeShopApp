461d5877f99fa2fbc45e7d08b5b9a4bcf54aa366
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-12-12 18:37:29 +0200

    Added api method

--- app/src/main/java/com/example/android/coffeeshopapp/api/CoffeeShopApi.java
+++ app/src/main/java/com/example/android/coffeeshopapp/api/CoffeeShopApi.java
@@ -1,8 +1,18 @@
 package com.example.android.coffeeshopapp.api;
 
+import com.example.android.coffeeshopapp.model.entities.ResponseEntity;
+
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+import rx.Observable;
+
 /**
  * Created by dev_serhii on 05.12.2017.
  */
 
 public interface CoffeeShopApi {
+
+    @GET("/user/getUser")
+    Observable<ResponseEntity> getUserData(@Query("userId") int id);
+
 }

30963696de32e90ef2002cb7ac7846fe282749b5
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-12-12 22:08:59 +0200

    Added Transactions Activity

---
+++ app/src/main/java/com/example/android/coffeeshopapp/ui/activities/TransactionsActivity.java
@@ -0,0 +1,22 @@
+package com.example.android.coffeeshopapp.ui.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.android.coffeeshopapp.R;
+
+/**
+ * Created by dev_serhii on 12.12.2017.
+ */
+
+public class TransactionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transactions);
+
+
+    }
+}

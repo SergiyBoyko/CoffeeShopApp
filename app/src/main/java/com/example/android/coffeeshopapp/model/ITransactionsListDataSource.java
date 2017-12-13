34ab3e2851443748f507afee80ab3f786196b118
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-12-13 14:40:27 +0200

    Added TransactionsListData source

---
+++ app/src/main/java/com/example/android/coffeeshopapp/model/ITransactionsListDataSource.java
@@ -0,0 +1,19 @@
+package com.example.android.coffeeshopapp.model;
+
+import com.example.android.coffeeshopapp.model.entities.PurchaseTransactionEntity;
+
+import java.util.List;
+
+import rx.Observable;
+
+/**
+ * Created by dev_serhii on 13.12.2017.
+ */
+
+public interface ITransactionsListDataSource {
+
+    Observable<List<PurchaseTransactionEntity>> getAllPurchases(long id);
+
+    Observable<List<PurchaseTransactionEntity>> getAllPurchasesForDay();
+
+}
---
+++ app/src/main/java/com/example/android/coffeeshopapp/model/remote/TransactionsListRemoteDataSource.java
@@ -0,0 +1,32 @@
+package com.example.android.coffeeshopapp.model.remote;
+
+import com.example.android.coffeeshopapp.api.CoffeeShopApi;
+import com.example.android.coffeeshopapp.model.ITransactionsListDataSource;
+import com.example.android.coffeeshopapp.model.entities.PurchaseTransactionEntity;
+
+import java.util.List;
+
+import rx.Observable;
+
+/**
+ * Created by dev_serhii on 13.12.2017.
+ */
+
+public class TransactionsListRemoteDataSource implements ITransactionsListDataSource {
+
+    private CoffeeShopApi coffeeShopApi;
+
+    public TransactionsListRemoteDataSource(CoffeeShopApi coffeeShopApi) {
+        this.coffeeShopApi = coffeeShopApi;
+    }
+
+    @Override
+    public Observable<List<PurchaseTransactionEntity>> getAllPurchases(long id) {
+        return coffeeShopApi.getAllPurchases(id);
+    }
+
+    @Override
+    public Observable<List<PurchaseTransactionEntity>> getAllPurchasesForDay() {
+        return coffeeShopApi.getAllPurchasesForDay();
+    }
+}

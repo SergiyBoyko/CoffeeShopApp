b3f70eafbff80baa3c30a7bc93b5b801f50e1062
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-12-13 14:35:04 +0200

    Added api methods

--- app/src/main/java/com/example/android/coffeeshopapp/api/CoffeeShopApi.java
+++ app/src/main/java/com/example/android/coffeeshopapp/api/CoffeeShopApi.java
@@ -1,6 +1,9 @@
 package com.example.android.coffeeshopapp.api;
 
 import com.example.android.coffeeshopapp.model.entities.ResponseEntity;
+import com.example.android.coffeeshopapp.model.entities.PurchaseTransactionEntity;
+
+import java.util.List;
 
 import okhttp3.ResponseBody;
 import retrofit2.http.GET;
@@ -19,6 +22,17 @@ public interface CoffeeShopApi {
 
     @GET("/user/getBalance")
     Observable<ResponseBody> getBalance(@Query("card_id") long id);
+
+    @GET("/purchase/getAllPurchases")
+    Observable<List<PurchaseTransactionEntity>> getAllPurchases(@Query("card_id") long id);
+
+    @GET("/purchase/getAllPurchasesForDay")
+    Observable<List<PurchaseTransactionEntity>> getAllPurchasesForDay();
+
+    @POST("/purchase/refund")
+    Observable<ResponseBody> refundTransaction(@Query("card_id") long cardId,
+                                               @Query("purchaseId") long purchaseId,
+                                               @Query("price") double price);
 
     @POST("/purchase/buy")
     Observable<ResponseBody> confirmTransaction(@Query("card_id") long id, @Query("price") double price);

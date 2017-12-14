67ef823e5ffb2ef690a193714c41072d9d9697f0
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-12-14 11:09:48 +0200

    Refactored Transaction activity

--- app/src/main/java/com/example/android/coffeeshopapp/ui/activities/TransactionsActivity.java
+++ app/src/main/java/com/example/android/coffeeshopapp/ui/activities/TransactionsActivity.java
@@ -27,6 +27,7 @@ import com.example.android.coffeeshopapp.views.TransactionListView;
 import com.example.android.coffeeshopapp.widgets.adapters.TransactionListAdapter;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -51,6 +52,9 @@ public class TransactionsActivity extends AppCompatActivity
 
     private TransactionListAdapter adapter;
 
+    private String filters;
+    private long userId;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -67,28 +71,23 @@ public class TransactionsActivity extends AppCompatActivity
         refundPresenter.setView(this);
         transactionListPresenter.setView(this);
 
-        String filters = getIntent().getStringExtra(Constants.PURCHASE_LIST);
-        List<PurchaseTransactionEntity> list = null;
-        if (filters.equals(Constants.ALL_USERS)) {
-//            transactionListPresenter.getAllPurchasesForDay();
-            list = loadTestData();
-        } else {
-            long id = getIntent().getLongExtra(Constants.CARD_ID, 0);
-//            transactionListPresenter.getAllPurchases(id);
-            list = new ArrayList<>();
-            PurchaseTransactionEntity p1 = new PurchaseTransactionEntity();
-            p1.setCardId(id);
-            p1.setDate(1513036800000L);
-            p1.setId(100L);
-            p1.setPrice(1000.50);
-            list.add(p1);
-        }
+        filters = getIntent().getStringExtra(Constants.PURCHASE_LIST);
+        userId = getIntent().getLongExtra(Constants.CARD_ID, 0);
 
-        adapter = new TransactionListAdapter(getContext(), list, this);
+        adapter = new TransactionListAdapter(getContext(), null, this);
         recyclerView.setAdapter(adapter);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        loadTransactionList();
+    }
+
+    private void loadTransactionList() {
+        if (filters.equals(Constants.ALL_USERS)) {
+            transactionListPresenter.getAllPurchasesForDay();
+        } else {
+            transactionListPresenter.getAllPurchases(userId);
+        }
     }
 
     @Override
@@ -98,6 +97,7 @@ public class TransactionsActivity extends AppCompatActivity
 
     @Override
     public void showTransactionList(List<PurchaseTransactionEntity> list) {
+        Collections.reverse(list);
         adapter.setTransactionList(list);
         adapter.notifyDataSetChanged();
     }
@@ -105,6 +105,7 @@ public class TransactionsActivity extends AppCompatActivity
     @Override
     public void onRefundSuccess(String message) {
         showText(message);
+        loadTransactionList();
     }
 
     @Override
@@ -114,38 +115,35 @@ public class TransactionsActivity extends AppCompatActivity
 
     @Override
     public void onRefundClicked(long cardId, long purchaseId) {
-//        showText("refund " + purchaseId);
         AlertDialog.Builder alert = new AlertDialog.Builder(this);
         final EditText edittext = new EditText(getContext());
         edittext.setInputType(InputType.TYPE_NUMBER_FLAG_DECIMAL);
         edittext.setTextColor(getResources().getColor(R.color.colorDarkGrey));
         alert.setMessage("Enter refund amount for " + cardId);
-        alert.setTitle("Refund");
+        alert.setTitle(getResources().getString(R.string.refund_title));
 
         alert.setView(edittext);
 
-        alert.setPositiveButton("Confirm Refund", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int whichButton) {
-                if (!InternetConnectivityUtil.isConnected(getContext())) {
-                    onRefundFailed(getResources().getString(R.string.network_problems));
-                    return;
-                }
-                String youEditTextValue = edittext.getText().toString();
-                showText(youEditTextValue);
-                try {
-                    double amount = Double.parseDouble(youEditTextValue);
-//                    refundPresenter.refundTransaction(cardId, purchaseId, amount);
-                    showText("sum " + amount);
-                } catch (RuntimeException e) {
-                    onRefundFailed("Incorrect value");
-                }
+        alert.setPositiveButton(getResources().getString(R.string.refund_confirm), (dialog, whichButton) -> {
+            if (!InternetConnectivityUtil.isConnected(getContext())) {
+                onRefundFailed(getResources().getString(R.string.network_problems));
+                return;
+            }
+            String youEditTextValue = edittext.getText().toString();
+            if (youEditTextValue.contains(",")) {
+                onRefundFailed(getResources().getString(R.string.use_dot));
+                return;
+            }
+            try {
+                double amount = Double.parseDouble(youEditTextValue);
+                refundPresenter.refundTransaction(cardId, purchaseId, amount);
+            } catch (RuntimeException e) {
+                onRefundFailed(getResources().getString(R.string.incorrect_value));
             }
         });
 
-        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int whichButton) {
-                // what ever you want to do with No option.
-            }
+        alert.setNegativeButton(getResources().getString(R.string.cancel), (dialog, whichButton) -> {
+            // what ever you want to do with No option.
         });
 
         alert.show();
@@ -157,34 +155,5 @@ public class TransactionsActivity extends AppCompatActivity
 
     public AppComponent getAppComponent() {
         return ((AppCoffeeShop) getApplication()).appComponent();
-    }
-
-    private List<PurchaseTransactionEntity> loadTestData() {
-        List<PurchaseTransactionEntity> list = new ArrayList<>();
-        PurchaseTransactionEntity p1 = new PurchaseTransactionEntity();
-        p1.setCardId(12345L);
-        p1.setDate(1513036800000L);
-        p1.setId(1L);
-        p1.setPrice(101.50);
-        list.add(p1);
-        PurchaseTransactionEntity p2 = new PurchaseTransactionEntity();
-        p2.setCardId(22345L);
-        p2.setDate(1513036800000L);
-        p2.setId(2L);
-        p2.setPrice(102.55);
-        list.add(p2);
-        PurchaseTransactionEntity p3 = new PurchaseTransactionEntity();
-        p3.setCardId(32345L);
-        p3.setDate(1513036800000L);
-        p3.setId(3L);
-        p3.setPrice(200D);
-        list.add(p3);
-        list.add(p1);
-        list.add(p2);
-        list.add(p3);
-        list.add(p1);
-        list.add(p2);
-        list.add(p3);
-        return list;
     }
 }

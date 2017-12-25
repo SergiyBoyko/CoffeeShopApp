1dc865ffbe93236da025778de82f309e537db848
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-12-25 20:15:22 +0200

    Fixed RefundView

--- app/src/main/java/com/example/android/coffeeshopapp/views/RefundView.java
+++ app/src/main/java/com/example/android/coffeeshopapp/views/RefundView.java
@@ -1,13 +1,17 @@
 package com.example.android.coffeeshopapp.views;
 
+import com.example.android.coffeeshopapp.model.entities.PurchaseTransactionEntity;
+
 /**
  * Created by dev_serhii on 13.12.2017.
  */
 
 public interface RefundView extends BaseView {
 
-    void onRefundSuccess(String message);
+    void onRefundSuccess(PurchaseTransactionEntity transactionEntity);
 
     void onRefundFailed(String message);
+
+    void onPinVerifySuccess(String message);
 
 }

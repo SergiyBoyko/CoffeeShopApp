1be503955cb5dad65a6d6870ee7ab6feb6b9ab81
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-12-26 17:48:40 +0200

    Added methods in reportView

--- app/src/main/java/com/example/android/coffeeshopapp/views/ZXReportView.java
+++ app/src/main/java/com/example/android/coffeeshopapp/views/ZXReportView.java
@@ -1,8 +1,17 @@
 package com.example.android.coffeeshopapp.views;
 
+import com.example.android.coffeeshopapp.model.entities.PurchaseTransactionEntity;
+
+import java.util.List;
+
 /**
  * Created by dev_serhii on 25.12.2017.
  */
 
 public interface ZXReportView extends BaseView {
+
+    void showZReport(List<PurchaseTransactionEntity> transactionEntities, long lastTimeUpdate);
+
+    void showXReport(List<PurchaseTransactionEntity> transactionEntities, long lastTimeUpdate);
+
 }

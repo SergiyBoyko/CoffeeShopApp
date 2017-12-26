e2e316cc6c97fa25a0d7632ddbf24d021815f6bc
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-12-26 17:50:09 +0200

    Added Reports view in MainActivity

--- app/src/main/java/com/example/android/coffeeshopapp/ui/activities/MainActivity.java
+++ app/src/main/java/com/example/android/coffeeshopapp/ui/activities/MainActivity.java
@@ -6,13 +6,13 @@ import android.content.Intent;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.text.InputType;
-import android.text.method.KeyListener;
-import android.view.KeyEvent;
-import android.view.WindowManager;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
@@ -27,17 +27,26 @@ import com.example.android.coffeeshopapp.common.Constants;
 import com.example.android.coffeeshopapp.di.component.AppComponent;
 import com.example.android.coffeeshopapp.di.component.DaggerPresentersComponent;
 import com.example.android.coffeeshopapp.di.module.PresentersModule;
+import com.example.android.coffeeshopapp.model.entities.PurchaseTransactionEntity;
 import com.example.android.coffeeshopapp.model.entities.ResponseEntity;
 import com.example.android.coffeeshopapp.presenters.UserInfoPresenter;
+import com.example.android.coffeeshopapp.presenters.ZXReportPresenter;
 import com.example.android.coffeeshopapp.utils.InternetConnectivityUtil;
 import com.example.android.coffeeshopapp.views.UserInfoView;
+import com.example.android.coffeeshopapp.views.ZXReportView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity implements KeyboardWatcher.OnKeyboardToggleListener, UserInfoView {
+public class MainActivity extends AppCompatActivity implements KeyboardWatcher.OnKeyboardToggleListener,
+        UserInfoView, ZXReportView {
 
     // Find out our editable field.
     @BindView(R.id.card_id)
@@ -49,11 +58,19 @@ public class MainActivity extends AppCompatActivity implements KeyboardWatcher.O
     @BindView(R.id.but_enter)
     Button enterButton;
 
+    @BindView(R.id.x_report_but)
+    Button xButton;
+    @BindView(R.id.z_report_but)
+    Button zButton;
+
     @BindView(R.id.view_all_trans_but)
     Button viewAllTransButton;
 
     @Inject
-    UserInfoPresenter presenter;
+    UserInfoPresenter userInfoPresenter;
+
+    @Inject
+    ZXReportPresenter reportPresenter;
 
     private KeyboardWatcher keyboardWatcher;
 
@@ -61,6 +78,8 @@ public class MainActivity extends AppCompatActivity implements KeyboardWatcher.O
 
     private int originalInputType;
     private boolean isKeyboardLock;
+
+    String uniqueId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,18 +94,11 @@ public class MainActivity extends AppCompatActivity implements KeyboardWatcher.O
                 .build()
                 .inject(this);
 
-        presenter.setView(this);
-        //865099026032140
-        //d9e76c56e5d68c1c  f4 ef e9 94 96 ff
-        TelephonyManager tManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-        String uid1 = tManager.getDeviceId();
-        String uid2 = Settings.Secure.getString(getApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID);
-        WifiManager manager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-        WifiInfo info = manager.getConnectionInfo();
-        String address = info.getMacAddress();
-        String s = "1)DeviceId " + uid1 + "\n2)AndroidId " + uid2 + "\n3)mac: " + address;
-        ((TextView)findViewById(R.id.textView1)).setText(s);
-        showText(s);
+        userInfoPresenter.setView(this);
+        reportPresenter.setView(this);
+        uniqueId = Settings.Secure.getString(getApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID);
+        String mainMessage = "Unique ID: " + uniqueId;
+        ((TextView) findViewById(R.id.textView1)).setText(mainMessage);
 
         isKeyboardLock = true;
         editText.requestFocus();
@@ -109,6 +121,8 @@ public class MainActivity extends AppCompatActivity implements KeyboardWatcher.O
         progressDialog = new ProgressDialog(this);
         progressDialog.setIndeterminate(true);
         progressDialog.setMessage(getResources().getString(R.string.verifying_progress));
+
+        setXZReportButtonsListeners();
     }
 
     @Override
@@ -153,6 +167,68 @@ public class MainActivity extends AppCompatActivity implements KeyboardWatcher.O
         progressDialog.hide();
     }
 
+    @Override
+    public void showZReport(List<PurchaseTransactionEntity> transactionEntities, long lastTimeUpdate) {
+        progressDialog.hide();
+        showReport(transactionEntities, lastTimeUpdate, Constants.Z_REPORT);
+    }
+
+    @Override
+    public void showXReport(List<PurchaseTransactionEntity> transactionEntities, long lastTimeUpdate) {
+        progressDialog.hide();
+        showReport(transactionEntities, lastTimeUpdate, Constants.X_REPORT);
+    }
+
+    private void showReport(List<PurchaseTransactionEntity> transactionEntities, long lastTimeUpdate, String title) {
+        Date date = new Date(lastTimeUpdate);
+        SimpleDateFormat dateFormat = new SimpleDateFormat(Constants.DATE_FORMAT, Locale.ENGLISH);
+        double totalAmount = 0;
+        StringBuilder receiptMessage = new StringBuilder(title + "\nUnique device id: " + uniqueId +
+                "\nLast time update: " + dateFormat.format(date));
+        for (PurchaseTransactionEntity entity : transactionEntities) {
+            receiptMessage
+                    .append("\nDate: ").append(dateFormat.format(new Date(entity.getDate())))
+                    .append(" ID: ").append(entity.getBadgeId())
+                    .append(" Amount: ").append(String.format(Locale.ENGLISH, "%.2f", entity.getPrice()));
+            totalAmount += entity.getPrice();
+        }
+        receiptMessage.append("\nTotal: ").append(String.format(Locale.ENGLISH, "%.2f", totalAmount));
+
+        View promptView = LayoutInflater.from(this).inflate(R.layout.zx_report_dialog, null);
+        TextView message = (TextView) promptView.findViewById(R.id.textmsg);
+        message.setText(receiptMessage);
+
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setView(promptView);
+        alert.setTitle(title);
+
+        alert.setPositiveButton(getResources().getString(R.string.print_receipt),
+                (dialog, whichButton) -> showText(getString(R.string.coming_soon)));
+
+        alert.setNegativeButton(getResources().getString(R.string.close_receipt), (dialog, whichButton) -> {
+            // what ever you want to do with No option.
+        });
+        alert.show();
+    }
+
+    private void setXZReportButtonsListeners() {
+        xButton.setOnClickListener(view -> {
+            if (InternetConnectivityUtil.isConnected(this)) {
+                progressDialog.show();
+                reportPresenter.getXReportWithLastTimeUpdate(uniqueId);
+            }
+            else showText(getString(R.string.network_problems));
+        });
+
+        zButton.setOnClickListener(view -> {
+            if (InternetConnectivityUtil.isConnected(this)) {
+                progressDialog.show();
+                reportPresenter.getZReportWithLastTimeUpdate(uniqueId);
+            }
+            else showText(getString(R.string.network_problems));
+        });
+    }
+
     private void startRoomActivity(String cardId, Double balance, String fullName) {
         Intent intent = new Intent(this, RoomActivity.class);
 
@@ -184,7 +260,7 @@ public class MainActivity extends AppCompatActivity implements KeyboardWatcher.O
 
         progressDialog.show();
 
-        presenter.getUserData(editText.getText().toString());
+        userInfoPresenter.getUserData(editText.getText().toString());
     }
 
     private void unlockKeyboard() {
@@ -214,27 +290,30 @@ public class MainActivity extends AppCompatActivity implements KeyboardWatcher.O
 
     /**
      * Method for showing the Keyboard
-     * @param context The context of the activity
+     *
+     * @param context  The context of the activity
      * @param editText The edit text for which we want to show the keyboard
      */
-    public void showTheKeyboard(Context context, EditText editText){
-        InputMethodManager imm = (InputMethodManager)context.getSystemService(Context.INPUT_METHOD_SERVICE);
+    public void showTheKeyboard(Context context, EditText editText) {
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT);
     }
 
     /**
      * Method for hiding the Keyboard
-     * @param context The context of the activity
+     *
+     * @param context  The context of the activity
      * @param editText The edit text for which we want to hide the keyboard
      */
-    public void hideTheKeyboard(Context context, EditText editText){
-        InputMethodManager imm = (InputMethodManager)context.getSystemService(Context.INPUT_METHOD_SERVICE);
+    public void hideTheKeyboard(Context context, EditText editText) {
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(editText.getWindowToken(), InputMethodManager.RESULT_UNCHANGED_SHOWN);
     }
+
     /**
      * Another method to hide the keyboard if the above method is not working.
      */
-    public void hideTheKeyboardSecond(EditText editText){
+    public void hideTheKeyboardSecond(EditText editText) {
         editText.setInputType(InputType.TYPE_NULL);
     }
 }

b988d2a20faf2fb0c21e2ad3e4dad72a57fc3f51
SergiyBoyko <developer.sergiyboyko@gmail.com>
2018-01-08 10:10:40 +0200

    Added printer utils

---
+++ app/src/main/java/com/example/android/coffeeshopapp/utils/printer/PrinterCallback.java
@@ -0,0 +1,10 @@
+package com.example.android.coffeeshopapp.utils.printer;
+
+/**
+ * Created by dev_serhii on 06.01.2018.
+ */
+
+public interface PrinterCallback {
+    String getResult();
+    void onReturnString(String result);
+}
